d537238488be693cdbbcf2f1998696e76eb46f22
agayushh <agayush088@gmail.com>
2025-09-09 00:35:54 +0530

    Refactor background script message listener parameters for clarity

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 console.log("Background script loaded");
 
-chrome.runtime.onMessage.addListener((msg, send, sendResponse) => {
+chrome.runtime.onMessage.addListener((msg, _send, _sendResponse) => {
   if (msg.action === "fillForm") {
     console.log("Filling form...");
   }
