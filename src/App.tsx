a744cbc495765ba8dfb9aed184cc17a915404fda
agayushh <agayush088@gmail.com>
2025-09-09 00:34:48 +0530

    Add Options component for managing saved datasets and remove App component

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div>hi there</div>;
-};
-
-export default App;
---
+++ src/Options.tsx
@@ -0,0 +1,8 @@
+export default function Options() {
+  return (
+    <div className="p-4">
+      <h1 className="text-lg font-bold">Options / Dataset</h1>
+      <p>Here you can manage your saved dataset.</p>
+    </div>
+  );
+}
